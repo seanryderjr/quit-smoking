d4c665fc7db30509c5f46e16319b2cfe129aa87d
Sean Ryder <sean.ryderjr@gmail.com>
2023-04-13 09:46:50 -0400

    s

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function calculate() {
 	// Get the quit date and number of cigarettes per day from the input fields
 	var quitDate = document.getElementById("quitDate").value;
 	var numPacks = document.getElementById("numPacks").value;
+    var pricePack = document.getElementById("pricePack").value;
 
 	// Calculate the number of days since the quit date
 	var today = new Date();
@@ -11,7 +12,19 @@ function calculate() {
 	// Calculate the number of cigarettes not smoked
 	var cigarettesNotSmoked = daysSinceQuit * numPacks * 20;
 
+    // Calculate the money saved
+    var moneySaved = numPacks * pricePack * daysSinceQuit;
+
 	// Display the results
 	var result = document.getElementById("result");
-	result.innerHTML = "Congratulations! You have not smoked " + cigarettesNotSmoked + " cigarettes since your quit date.";
+	result.innerHTML = `Congratulations! You have not smoked ${cigarettesNotSmoked} cigarettes since your quit date.`;
+
+    var result2 = document.getElementById("result2");
+    result2.innerHTML = `Congratulations! You have saved $${moneySaved} since your quit date.`;
+
+    var result3 = document.getElementById("result3");
+
+    var result4 = document.getElementById("result4");
+
+
 }
